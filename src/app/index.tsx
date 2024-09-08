8f73a046715638d5bb22c1e8142c321052f89ae2
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-09-08 08:53:33 -0700

    add: Docker File

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello form the App!</h1>
+      <h1 className="text-3xl font-bold">
+        Hello form the App  <span className="text-red-500">Another staging</span>!
+      </h1>
     </>
   );
 }
