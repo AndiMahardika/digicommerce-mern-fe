7ce056d8cd7cde7c055e4d818b4b40c287455617
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-09-08 09:53:32 -0700

    add: change staging

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">
-        Hello form the App  <span className="text-red-500">Another staging</span>!
+        Hello form the App  <span className="text-red-500">Update staging</span>!
       </h1>
     </>
   );
