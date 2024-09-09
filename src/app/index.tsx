ff7b5ca348624ec901c4857a16831883f953b8a2
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-09-09 08:29:02 -0700

    add: design system, components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,19 @@
+import { Mail } from "lucide-react";
+import { Button } from "../components/ui/button";
+import { Tooltip } from "../components/ui/tooltip";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">
+      <h1 className="text-5xl font-bold">
         Hello form the App  <span className="text-red-500">Update staging</span>!
       </h1>
+      <Tooltip message="Hello from tooltip">
+        <Button variant="outline" startContent={<Mail />}>
+          Hello!
+        </Button>
+      </Tooltip>
+      {/* <Button startContent={<LogIn size={16}/>} size="small">Login</Button> */}
     </>
   );
 }
